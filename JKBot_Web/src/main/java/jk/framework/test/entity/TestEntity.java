58f22ed22085246585313356fd35072bad499de0
wonjongkyu <wonjongkyu@naver.com>
2018-03-23 10:52:08 +0900

    init Project

---
+++ JKBot_Web/src/main/java/jk/framework/test/entity/TestEntity.java
@@ -0,0 +1,14 @@
+package jk.framework.test.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+public class TestEntity {
+	private int bno;
+	private String userName;
+	private String contents;
+}
