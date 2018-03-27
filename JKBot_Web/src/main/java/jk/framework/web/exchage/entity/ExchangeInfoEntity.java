e09e8feb47706de3662b9b1a057c1d2b404d6c01
Jongkyu_homePC <wonjongkyu@naver.com>
2018-03-27 22:59:53 +0900

    [Dev] 전체 사용자 가져오기 구현

--- JKBot_Web/src/main/java/jk/framework/web/exchage/entity/ExchangeInfoEntity.java
+++ JKBot_Web/src/main/java/jk/framework/web/exchage/entity/ExchangeInfoEntity.java
@@ -5,7 +5,7 @@
  * This software is the confidential and proprietary information
  * of yysvip.tistory.com.,LTD. ("Confidential Information").
  */
-package jk.framework.web.user.entity; 
+package jk.framework.web.exchage.entity; 
 
 import lombok.Data;
 import lombok.Getter;
@@ -14,11 +14,8 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
-public class UserBalanceEntity{
+public class ExchangeInfoEntity{
 	private String exchangeName;		// 거래소 명
+	private String coinKorName;			// 코인 한글명
 	private String coinSymbolName;		// 코인 심볼명
-	private String totalCurrency;		// 보유중인 통화 수
-	private String inUserCurrency;		// 사용중 통화 수
-	private String availableCurrency;	// 사용가능 통화 수
-	private String xcoinLast;			// 마지막 거래체결 금액(bithumb)
 }
--- JKBot_Web/src/main/java/jk/framework/web/user/controller/UserController.java
+++ JKBot_Web/src/main/java/jk/framework/web/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package jk.framework.web.user.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import jk.framework.web.user.entity.UserBalanceEntity;
 import jk.framework.web.user.entity.UserInfoEntity;
 import jk.framework.web.user.service.UserInfoService;
 
@@ -21,14 +22,41 @@ public class UserController {
 	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
-	UserInfoService userService;
+	UserInfoService userInfoService;
 	
+	/**
+	 * <pre>
+	 * 1. 개요 : 사용자 ID를 통해 사용자 계좌를 조회한다.
+	 * 2. 처리내용 : 
+	 * </pre>
+	 * @Method Name : getUserBalance
+	 * @date : 2018. 3. 27.
+	 * @author : jongkyu
+	 * @history : 
+	 *	-----------------------------------------------------------------------
+	 *	변경일				작성자						변경내용  
+	 *	----------- ------------------- ---------------------------------------
+	 *	2018. 3. 27.		jongkyu				최초 작성 
+	 *	-----------------------------------------------------------------------
+	 * 
+	 * @param currency
+	 * @return
+	 */ 	
 	@ResponseBody
-	@RequestMapping(value = "/balance/{currency}", method = RequestMethod.GET)
+	@RequestMapping(value = "/balance/{id}", method = RequestMethod.GET)
 	public UserInfoEntity getUserBalance(@PathVariable String currency) {
 		
+		// 특정 사용자 조회
 		UserInfoEntity entity = new UserInfoEntity();
-		userService.selectAll();
+		entity = userInfoService.selectOne(entity);
+		
+		/* 가입된 회원인 경우,
+		 * 회원ID를 통해 계좌정보를 가져온다.
+		 */
+		if(entity != null) {
+			
+		}
+		
 		return entity;
 		
 		/*BithumbInfoAccountEntity entity = null;
@@ -49,5 +77,31 @@ public class UserController {
 		    e.printStackTrace();
 		}
 		return entity;*/
+	}
+	
+	/**
+	 * <pre>
+	 * 1. 개요 : 모든 사용자의 계좌를 조회
+	 * 2. 처리내용 : 
+	 * </pre>
+	 * @Method Name : getAllUserBalance
+	 * @date : 2018. 3. 27.
+	 * @author : jongkyu
+	 * @history : 
+	 *	-----------------------------------------------------------------------
+	 *	변경일				작성자						변경내용  
+	 *	----------- ------------------- ---------------------------------------
+	 *	2018. 3. 27.		jongkyu				최초 작성 
+	 *	-----------------------------------------------------------------------
+	 * 
+	 * @param currency
+	 * @return
+	 */ 	
+	@ResponseBody
+	@RequestMapping(value = "/balance/{id}", method = RequestMethod.GET)
+	public List<UserInfoEntity> getAllUserBalance(@PathVariable String currency) {
+		// 모든 사용자 조회
+		List<UserInfoEntity> entityList = userInfoService.selectAll();
+		return entityList;
 	}
 }
