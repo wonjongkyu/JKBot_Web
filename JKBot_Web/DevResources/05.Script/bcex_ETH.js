e40c52f61370ddf9f7c525bca173f70eebfe5c3c
wonjongkyu <wonjongkyu@naver.com>
2018-10-17 17:45:27 +0900

    no message

--- JKBot_Web/DevResources/05.Script/bcex_ETH.js
+++ JKBot_Web/DevResources/05.Script/bcex_ETH.js
@@ -1,16 +1,11 @@
 
-var proxyJsonp="https://script.google.com/macros/s/AKfycbwmqG55tt2d2FcT_WQ3WjCSKmtyFpkOcdprSITn45-4UgVJnzp9/exec";
-jQuery.ajaxOrig=jQuery.ajax;jQuery.ajax=function(a,b){function d(a){a=encodeURI(a).replace(/&/g,"%26");return proxyJsonp+"?url="+a+"&callback=?"}var c="object"===typeof a?a:b||{};c.url=c.url||("string"===typeof a?a:"");var c=jQuery.ajaxSetup({},c),e=function(a,c){var b=document.createElement("a");b.href=a;return c.crossOrigin&&"http"==a.substr(0,4).toLowerCase()&&"localhost"!=b.hostname&&"127.0.0.1"!=b.hostname&&b.hostname!=window.location.hostname}(c.url,c);c.proxy&&0<c.proxy.length&&(proxyJsonp=c.proxy,"object"===typeof a?
-a.crossDomain=!0:"object"===typeof b&&(b.crossDomain=!0));e&&("object"===typeof a?a.url&&(a.url=d(a.url),a.charset&&(a.url+="&charset="+a.charset),a.dataType="json"):"string"===typeof a&&"object"===typeof b&&(a=d(a),b.charset&&(a+="&charset="+b.charset),b.dataType="json"));return jQuery.ajaxOrig.apply(this,arguments)};jQuery.ajax.prototype=new jQuery.ajaxOrig;jQuery.ajax.prototype.constructor=jQuery.ajax;
-
 $(document).ready(function() {
 	
 	setInterval(function(){		// 0.5초마다 빗썸 이더리움 가격 호출
 		getBithumbEthPrice();
-	}, 1000);
+	}, 2000);
 	
 });
-
 
 //BTC-KRW 가격 저장
 function getBithumbEthPrice(){
@@ -20,9 +15,8 @@ function getBithumbEthPrice(){
 		url : 'https://api.bithumb.com/public/ticker/ETH',
 		dataType : 'json',
 		timeout : 50000,
-		crossOrigin: true,
 		success : function(data) {
-			var result = JSON.parse(data);
+			var result = data; //JSON.parse(data);
 			var price = result.data.closing_price;
 			
 			var etosi = $(".list-bot").children().find('.price');
@@ -34,10 +28,6 @@ function getBithumbEthPrice(){
  				$(this).next().text(krw_price);
 			});
  
-/* 			// div list-bot 클래스 밑에 .price 클래스 찾고, pir 클래스에 원화가격 넣어준다.
-			var etosi = $(".price").text();
-			console.log(etosi);
-			$(".price").append( etosi*price + '원'); */
 		},
 		error : function(e) {
 			console.log("ERROR: ", e);
